45c6bd95d222612127657f0bd1b25b75b6b7c331
hingkop <qkrtjswlswkd@naver.com>
2024-02-20 15:15:22 +0900

    first commit

---
+++ src/main/java/edu/fisa/lab/SimpleController.java
@@ -0,0 +1,16 @@
+package edu.fisa.lab;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Getter;
+
+@RestController
+public class SimpleController {
+	
+	@GetMapping("getdata")
+	public String get() {
+		System.out.println("***********");
+		return "초코송이 짱짱굿~";
+	}
+}
